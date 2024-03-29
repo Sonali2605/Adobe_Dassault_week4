5e105ab75e006d3974ed980a5bcbe4f9d1110dfc
sohelPathanHarbinger <165145647+sohelPathanHarbinger@users.noreply.github.com>
2024-03-29 15:28:42 +0530

    About Added

--- src/project-components/profilePage.tsx
+++ src/project-components/profilePage.tsx
@@ -28,6 +28,7 @@ const ProfilePage = () => {
   const [userBadges, setUserBadges] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editedName, setEditedName] = useState('');
+  const [about, setAbout] = useState('');
   const [editedAvatarUrl, setEditedAvatarUrl] = useState('');
 
   useEffect(() => {
@@ -40,6 +41,7 @@ const ProfilePage = () => {
         };
         const response = await axios.get('https://learningmanager.adobe.com/primeapi/v2/user', config);
         setUserData(response.data?.data);
+        setAbout(response.data?.data.attributes.bio);
       } catch (error) {
         console.error('Error fetching user data:', error);
       }
@@ -87,8 +89,11 @@ const ProfilePage = () => {
   const handleEdit = () => {
     setIsEditing(true);
     setEditedName(userData && userData.attributes && userData.attributes.name);
+    setAbout(userData && userData.attributes && userData.attributes.bio);
     setEditedAvatarUrl(userData && userData.attributes && userData.attributes.avatarUrl);
   };
+
+
 
   const handleSave = async () => {
     try {
@@ -101,7 +106,8 @@ const ProfilePage = () => {
           id: `${userData?.id}`,
           type: `${userData?.type}`,
           attributes: {
-            name: `${editedName}`
+            name: `${editedName}`,
+            bio: `${about}`
           }
         }
       };
@@ -126,13 +132,13 @@ const ProfilePage = () => {
       <div className='mb-5'>
         <Header isLogin={true} />
       </div>
-      <h1 style={{ fontSize: "25px", width: "100%", marginLeft: "10rem", marginBottom: "1rem"}}>User Profile</h1>
+      <h1 style={{ fontSize: "25px", width: "100%", marginLeft: "10rem", marginBottom: "1rem" }}>User Profile</h1>
       <div className="flex justify-center">
 
         <div className="border border-gray-300 rounded-lg overflow-hidden w-3/4 p-6" style={{ backgroundColor: "#fff" }}>
           <div className="flex items-center">
 
-            <div className="rounded-full overflow-hidden mr-4" style={{width:"200px"}}>
+            <div className="rounded-full overflow-hidden mr-4" style={{ width: "200px" }}>
               <img
                 src={isEditing ? editedAvatarUrl : (userData && userData.attributes && userData.attributes.avatarUrl)}
                 alt="Avatar"
@@ -153,30 +159,40 @@ const ProfilePage = () => {
               <p>{userData && userData.attributes && userData.attributes.profile}</p>
               {isEditing && (
                 <div>
-                  <button onClick={handleSave} className="px-4 py-2 bg-blue-500 text-white rounded-md mr-2"  style={{backgroundColor:"#005686"}}>Save</button>
+                  <button onClick={handleSave} className="px-4 py-2 bg-blue-500 text-white rounded-md mr-2" style={{ backgroundColor: "#005686" }}>Save</button>
                   <button onClick={() => setIsEditing(false)} className="px-4 py-2 bg-gray-500 text-white rounded-md">Cancel</button>
                 </div>
               )}
             </div>
             {!isEditing && (
-              <button onClick={handleEdit} className="ml-auto px-4 py-2 text-white rounded-md" style={{backgroundColor:"#005686"}}>Edit</button>
+              <button onClick={handleEdit} className="ml-auto px-4 py-2 text-white rounded-md" style={{ backgroundColor: "#005686" }}>Edit</button>
             )}
           </div>
+          <div className="bio-wrapper">
+            <div className="bio-title">
+              <h2>About</h2>
+            </div>
+            <div className="bio-text">
+              <textarea value={about} onChange={(e) => setAbout(e.target.value)} style={{ width: "100%", border: "2px solid #000", padding: "1px 4px" }} name="" id="about_bio" rows="4" disabled={!isEditing}></textarea>
+            </div>
+          </div>
           <div className="flex justify-between mt-6">
-            <div className="w-1/2 p-6" style={{border: "1px solid #e7e6e6",borderRadius: "8px", marginRight:"10px"}}>
+            <div className="w-1/2 p-6" style={{ border: "1px solid #e7e6e6", borderRadius: "8px", marginRight: "10px" }}>
               <h3 className="text-lg font-semibold mb-4">Skills</h3>
-              <ul style={{    listStyle: "disc",    padding: "0 0 0 15px",    lineHeight: "28px"}}>
+              <ul style={{ listStyle: "disc", padding: "0 0 0 15px", lineHeight: "28px" }}>
                 {userSkills.map((skill) => (
                   <li key={skill.id}>{skill.attributes.name}</li>
                 ))}
               </ul>
             </div>
-            <div className="w-1/2 p-6" style={{border: "1px solid #e7e6e6",borderRadius: "8px", marginLeft:"10px"}}>
+            <div className="w-1/2 p-6" style={{ border: "1px solid #e7e6e6", borderRadius: "8px", marginLeft: "10px" }}>
               <h3 className="text-lg font-semibold mb-4">Badges</h3>
               <div className="flex flex-wrap">
                 {userBadges.map((badge) => (
-                  <div key={badge.id} className="mb-4  ml-auto" style={{width:"100px"}}>
-                    <img src={badge.attributes.imageUrl} alt="Badge" className="w-full h-auto" />
+                  <div key={badge.id} className="mb-4  ml-auto" style={{ width: "100px" }}>
+                    <a href="https://in.linkedin.com" target='_blank' className="href" title='Lnked In'>
+                      <img src={badge.attributes.imageUrl} alt="Badge" className="w-full h-auto" />
+                    </a>
                   </div>
                 ))}
               </div>
