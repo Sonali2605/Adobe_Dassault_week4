3b644da9e30563098ea7a59d1a8ea5f11af51533
Sonali Dhiman <Sonali.Dhiman@harbingergroup.com>
2024-04-03 16:40:30 +0530

    profile page

--- src/AppConfig.tsx
+++ src/AppConfig.tsx
@@ -1,11 +1,11 @@
 // AppConfig.js
 export const base_url = "http://localhost:3000";
 export const base_adobe_url = "https://learningmanager.adobe.com";
-export const clientId= "449923a1-a01c-4bf5-b7c8-2137718d6d04"    
+export const clientId= "f8fe1dad-1699-4ced-b255-d02f991425e8"    
 // eabb3668-a036-45c5-ba10-7a4160827517";
-export const clientSecreat ="b1b22c3e-900c-4bd1-b010-daf95c01b968";
+export const clientSecreat ="2e355ebf-d866-4a1d-a7d3-5c0812ac9881";
 // "5ec25713-5718-4d71-91bd-c18f703b3407";
-export const refreshToken ="cfc9007046dac38146b4eae495192d3c"
+export const refreshToken ="56277af312864991e9e64d8a4f586fb5"
 //  "371390c98a8c3a4a24922adf11fb6b08";
-export const learnerToken = "10966576b968372756968ea3723d5fee";
+export const learnerToken = "46cd84deab11cc1acd805885894a61f4";
 export const cid = "course:9180283";
